a5e78079dc9b3126cb460b5d5ea71d24575e00c4
Muhammet Talha Kaymak <talha.kaymak@bil.omu.edu.tr>
2026-01-06 17:10:40 +0300

    feat : Updated  searchView

--- lib/view/search_view.dart
+++ lib/view/search_view.dart
@@ -1,29 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:spotify_clone/core/constants/app_colors.dart';
-import 'package:spotify_clone/core/constants/app_paddings.dart';
-import 'package:spotify_clone/core/constants/app_sizes.dart';
 import 'package:spotify_clone/core/constants/app_strings.dart';
+import 'package:spotify_clone/core/l10n/generated/app_localizations.dart';
 import 'package:spotify_clone/core/network/token_manager.dart';
 import 'package:spotify_clone/models/catogory_model.dart';
 import 'package:spotify_clone/view/search_detail_view.dart';
 import 'package:spotify_clone/view_model/search_view_model.dart';
+import 'package:spotify_clone/widgets/custom_widgets/app_icon.dart';
+import 'package:spotify_clone/widgets/custom_widgets/app_text.dart';
 
 class SearchView extends StatefulWidget {
   final GlobalKey<ScaffoldState> scaffoldKey;
   const SearchView({super.key, required this.scaffoldKey});
+
   @override
   State<SearchView> createState() => _SearchViewState();
 }
 
 class _SearchViewState extends State<SearchView> {
-  final EdgeInsetsGeometry paddingH10V5 = EdgeInsetsGeometry.symmetric(
-    horizontal: 10,
-    vertical: 5,
-  );
-  final double expandedHeight = 130;
-
   late SearchViewModel viewModel;
 
   @override
@@ -40,13 +37,11 @@ class _SearchViewState extends State<SearchView> {
         headerSliverBuilder: (context, innerBoxIsScrolled) {
           return [
             _SliverAppBar(
-              expandedHeight: expandedHeight,
-              widget: widget,
-              paddingH10V5: paddingH10V5,
+              scaffoldKey: widget.scaffoldKey,
+              innerBoxIsScrolled: innerBoxIsScrolled,
             ),
           ];
         },
-
         body: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
@@ -61,206 +56,128 @@ class _SearchViewState extends State<SearchView> {
                 },
               ),
             ),
+            SizedBox(height: 100.h,)
           ],
         ),
       ),
     );
   }
 
-  Padding _titleSection() {
+  Widget _titleSection() {
+    final l10n = AppLocalizations.of(context)!;
     return Padding(
-      padding: AppPaddings.all10,
-      child: Text(
-        AppStrings.searchViewTitle,
-        style: TextStyle(
-          color: AppColors.white,
-          fontSize: AppSizes.fontSize16,
-          fontWeight: FontWeight.w800,
-        ),
+      padding: _Constants.titleSectionPadding,
+      child: AppText(
+        text: l10n.searchViewTitle,
+        style: AppTextStyle.titleM,
+        fontWeight: FontWeight.w800,
+        color: AppColors.white,
       ),
     );
   }
 
   Widget _buildCategoryGrid() {
     return GridView.builder(
-      padding: const EdgeInsets.only(bottom: 150, left: 10, right: 10),
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        crossAxisSpacing: 10,
-        mainAxisSpacing: 10,
-        childAspectRatio: 2,
-      ),
+      padding: _Constants.gridPadding,
+      gridDelegate: _Constants.gridDelegate,
       itemCount: viewModel.itemsCategory.length,
       itemBuilder: (context, index) {
         final item = viewModel.itemsCategory[index];
-        return _CategoryCard(item);
+        return _CategoryCard(item: item);
       },
     );
   }
 }
 
-class _CategoryCard extends StatelessWidget {
-  final CategoryItem item;
-  const _CategoryCard(this.item);
-
-  final double containerHeight = 50;
-  final double imageSize = 90;
-
-  @override
-  Widget build(BuildContext context) {
-    final color = categoryColors[item.name] ?? AppColors.grey;
-
-    return Container(
-      height: containerHeight,
-      decoration: BoxDecoration(
-        color: color,
-        borderRadius: BorderRadius.circular(10),
-      ),
-      clipBehavior: Clip.hardEdge,
-      child: Stack(
-        children: [
-          Positioned(
-            right: -15,
-            bottom: -20,
-            child: Transform.rotate(
-              angle: 0.50,
-              child: Image.network(
-                item.imageUrl ?? "",
-                width: imageSize,
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-          Positioned(
-            top: 12,
-            left: 12,
-            child: Text(
-              item.name ?? "",
-              style: TextStyle(
-                color: AppColors.white,
-                fontSize: AppSizes.fontSize16,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-final Map<String, Color> categoryColors = {
-  "Made For You": Color(0xFF6111C4),
-  "Pop": Color(0xFFC026D2),
-  "New Releases": Color(0xFFE41E63),
-  "Hip-Hop": Color(0xFF0A6F5D),
-  "Rock": Color(0xFF0A6F5D),
-  "K-pop": Color(0xFF4971FF),
-  "Indie": Color(0xFFE60026),
-  "Dance/Electronic": Color(0xFFFF7900),
-  "Mood": Color(0xFF6A4C93),
-  "Charts": Color(0xFF456F75),
-  "Party": Color(0xFFFF5A36),
-  "Sleep": Color(0xFF007AFE),
-  "Chill": Color(0xFF8E44AE),
-  "At Home": Color(0xFF3E2C62),
-  "EQUAL": Color(0xFFAE27CC),
-  "Trending": Color(0xFF6111C4),
-  "Workout": Color(0xFFC026D2),
-  "Discover": Color(0xFF6111C4),
-  "Country": Color(0xFFC026D2),
-  "R&B": Color(0xFF6111C4),
-};
-
 class _SliverAppBar extends StatelessWidget {
+  final GlobalKey<ScaffoldState> scaffoldKey;
+  final bool innerBoxIsScrolled;
   const _SliverAppBar({
-    required this.expandedHeight,
-    required this.widget,
-    required this.paddingH10V5,
+    required this.scaffoldKey,
+    required this.innerBoxIsScrolled,
   });
 
-  final double expandedHeight;
-  final SearchView widget;
-  final EdgeInsetsGeometry paddingH10V5;
-
   @override
   Widget build(BuildContext context) {
+    final l10n = AppLocalizations.of(context)!;
     return SliverAppBar(
       floating: true,
       snap: false,
       pinned: true,
-      expandedHeight: expandedHeight,
+      elevation: 0,
+      scrolledUnderElevation: 0, 
+      forceElevated: innerBoxIsScrolled,
+      expandedHeight: _Constants.appBarExpandedHeight,
       backgroundColor: AppColors.black,
+      leadingWidth: _Constants.leadingWidth,
       leading: Padding(
-        padding: AppPaddings.leadingPadding,
+        padding: _Constants.leadingPadding,
         child: GestureDetector(
-          onTap: () async{
-            TokenManager().invalidateToken();
-            print("-------------------------------------------");
-            final token = await TokenManager().getToken();
-print("Token: $token");
-            print("-------------------------------------------");
-            //---------------------------------------------------------------------------------------------------------
-            widget.scaffoldKey.currentState!.openDrawer();
+          onTap: () async {
+            await TokenManager().invalidateToken();
+            scaffoldKey.currentState!.openDrawer();
           },
           child: Container(
-            width: AppSizes.avatarSize2,
-            height: AppSizes.avatarSize2,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              color: AppColors.blackPanther,
+            decoration: _Constants.profileImageDecoration,
+            child: Center(
+              child: ClipOval(
+                child: Image.asset(
+                  AppStrings.profileImagePath,
+                  fit: BoxFit.cover,
+                ),
+              ),
             ),
-            child: Center(child: Image.asset(AppStrings.profileImagePath)),
           ),
         ),
       ),
-      title: Text(AppStrings.search),
+      title: AppText(
+        text: l10n.bottomNavigatorSearch,
+        style: AppTextStyle.titleL,
+        fontWeight: FontWeight.bold,
+        color: AppColors.white,
+      ),
       actions: [
         Padding(
-          padding: AppPaddings.right10,
+          padding: _Constants.actionPadding,
           child: IconButton(
-            color: AppColors.white,
             onPressed: () {},
-            icon: FaIcon(FontAwesomeIcons.camera),
+            icon: AppIcon(
+              icon: FontAwesomeIcons.camera,
+              color: AppColors.white,
+              size: AppIconSize.medium,
+            ),
           ),
         ),
       ],
       bottom: PreferredSize(
-        preferredSize: Size.fromHeight(60),
+        preferredSize: _Constants.searchBarPreferredSize,
         child: Padding(
-          padding: paddingH10V5,
+          padding: _Constants.searchBarPadding,
           child: GestureDetector(
             onTap: () {
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => SearchDetailView()),
+                MaterialPageRoute(builder: (context) => const SearchDetailView()),
               );
             },
             child: Container(
-              height: 50,
-              decoration: BoxDecoration(
-                color: AppColors.white,
-                borderRadius: BorderRadius.all(Radius.circular(10)),
-              ),
+              height: _Constants.searchBarHeight,
+              decoration: _Constants.searchBarDecoration,
               child: Row(
                 children: [
                   Padding(
-                    padding: AppPaddings.all10,
-                    child: Icon(
-                      Icons.search,
+                    padding: _Constants.searchIconPadding,
+                    child: AppIcon(
+                      icon: Icons.search,
                       color: AppColors.black,
-                      size: AppSizes.size30,
+                      size: AppIconSize.large,
                     ),
                   ),
-                  Padding(
-                    padding: AppPaddings.left10,
-                    child: Text(
-                      AppStrings.searchBarTitle,
-                      style: TextStyle(
-                        color: AppColors.black,
-                        fontSize: AppSizes.fontSize16,
-                      ),
-                    ),
+                  AppText(
+                    text: l10n.searchViewSearchBarText,
+                    style: AppTextStyle.bodyM,
+                    color: AppColors.black,
+                    fontWeight: FontWeight.w600,
                   ),
                 ],
               ),
@@ -270,4 +187,112 @@ print("Token: $token");
       ),
     );
   }
+}
+
+class _CategoryCard extends StatelessWidget {
+  final CategoryItem item;
+  const _CategoryCard({required this.item});
+
+  @override
+  Widget build(BuildContext context) {
+    final color = categoryColors[item.name] ?? AppColors.grey;
+    return Container(
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: _Constants.cardBorderRadius,
+      ),
+      clipBehavior: Clip.hardEdge,
+      child: Stack(
+        children: [
+          Positioned(
+            right: _Constants.imagePositionRight,
+            bottom: _Constants.imagePositionBottom,
+            child: Transform.rotate(
+              angle: _Constants.imageRotationAngle,
+              child: Image.network(
+                item.imageUrl ?? "",
+                width: _Constants.categoryImageSize,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Positioned(
+            top: _Constants.categoryTitlePosition,
+            left: _Constants.categoryTitlePosition,
+            child: AppText(
+              text: item.name ?? "",
+              style: AppTextStyle.titleS,
+              fontWeight: FontWeight.bold,
+              color: AppColors.white,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+final Map<String, Color> categoryColors = {
+  "Made For You": const Color(0xFF6111C4),
+  "Pop": const Color(0xFFC026D2),
+  "New Releases": const Color(0xFFE41E63),
+  "Hip-Hop": const Color(0xFF0A6F5D),
+  "Rock": const Color(0xFF0A6F5D),
+  "K-pop": const Color(0xFF4971FF),
+  "Indie": const Color(0xFFE60026),
+  "Dance/Electronic": const Color(0xFFFF7900),
+  "Mood": const Color(0xFF6A4C93),
+  "Charts": const Color(0xFF456F75),
+  "Party": const Color(0xFFFF5A36),
+  "Sleep": const Color(0xFF007AFE),
+  "Chill": const Color(0xFF8E44AE),
+  "At Home": const Color(0xFF3E2C62),
+  "EQUAL": const Color(0xFFAE27CC),
+  "Trending": const Color(0xFF6111C4),
+  "Workout": const Color(0xFFC026D2),
+  "Discover": const Color(0xFF6111C4),
+  "Country": const Color(0xFFC026D2),
+  "R&B": const Color(0xFF6111C4),
+};
+
+abstract final class _Constants {
+  // Size
+  static double get appBarExpandedHeight => 120.h;
+  static double get searchBarHeight => 46.h;
+  static Size get searchBarPreferredSize => Size.fromHeight(56.h);
+  static double get categoryImageSize => 90.w;
+  static double get leadingWidth => 55.w;
+
+  // Grid
+  static SliverGridDelegate get gridDelegate => const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+        childAspectRatio: 2,
+      );
+
+  // Position & Rotation
+  static double get imageRotationAngle => 0.50;
+  static double get imagePositionRight => -15.w;
+  static double get imagePositionBottom => -20.h;
+  static double get categoryTitlePosition => 12.w;
+
+  // Padding
+  static EdgeInsetsGeometry get titleSectionPadding => EdgeInsets.all(12.w);
+  static EdgeInsetsGeometry get gridPadding => EdgeInsets.only(bottom: 100.h, left: 12.w, right: 12.w);
+  static EdgeInsetsGeometry get leadingPadding => EdgeInsetsDirectional.only(start: 12.w);
+  static EdgeInsetsGeometry get actionPadding => EdgeInsetsDirectional.only(end: 10.w);
+  static EdgeInsetsGeometry get searchBarPadding => EdgeInsetsDirectional.fromSTEB(12.w, 0, 12.w, 10.h);
+  static EdgeInsetsGeometry get searchIconPadding => EdgeInsets.symmetric(horizontal: 10.w);
+
+  // Decoration
+  static BorderRadius get cardBorderRadius => BorderRadius.circular(10.r);
+  static BoxDecoration get searchBarDecoration => BoxDecoration(
+        color: AppColors.white,
+        borderRadius: BorderRadius.all(Radius.circular(8.r)),
+      );
+  static BoxDecoration get profileImageDecoration => BoxDecoration(
+        shape: BoxShape.circle,
+       // color: AppColors.blackPanther,
+      );
 }
